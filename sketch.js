a2e04eca3c72212816a7438ae3e4253b24c284dd
Rashmi Deshmukh <57807833+rashmi-deshmukh@users.noreply.github.com>
2021-07-26 15:24:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,17 +16,6 @@ bg = createSprite(165,485,1,1);
 bg.addImage(bgImg);
 bg.scale = 1.3
 
-//creating top and bottom grounds
-bottomGround = createSprite(200,390,800,20);
-bottomGround.visible = false;
-
-topGround = createSprite(200,10,800,20);
-topGround.visible = false;
-      
-//creating balloon     
-balloon = createSprite(100,200,20,50);
-balloon.addAnimation("balloon",balloonImg);
-balloon.scale = 0.2;
 
 
 
@@ -37,13 +26,7 @@ function draw() {
   background("black");
         
           //making the hot air balloon jump
-          if(keyDown("space")) {
-            balloon.velocityY = -6 ;
-            
-          }
 
-          //adding gravity
-           balloon.velocityY = balloon.velocityY + 2;
    
         drawSprites();
         
